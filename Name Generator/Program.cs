2ba2b52fa9c8b15039ef142a60222c5a1a2f9fbc
Vasko <b.vasko@abv.bg>
2021-03-08 20:02:57 +0200

    start

---
+++ Name Generator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;  
+using Newtonsoft.Json.Linq;  
+
+namespace Name_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class RandomName //Class used to generate a random name
+    {
+        class NameList //Class for holding the lists of names from names.json
+        {
+            public string[] boys { get; set; }
+            public string[] girls { get; set; }
+            public string[] last { get; set; }
+        }
+    }
+}
